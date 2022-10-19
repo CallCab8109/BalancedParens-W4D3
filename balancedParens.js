ba6bae2c84131785ed27e3f59b02e5551c1f0331
Carter Bleyl <Carter.Bleyl@gmail.com>
2022-10-19 10:15:30 -0600

    done, may add more later

---
+++ balancedParens.js
@@ -0,0 +1,28 @@
+// Sample Strings
+let sample1 = "This ( is unbalanced."
+let sample2 = "(This (is (a) balanced) string.)"
+let sample3 = "This is () also ) unbalanced."
+let sample4 = "Balanced."
+
+// Write your solution below:
+function countString(str) {
+    let count = 0
+    let count2 = 0
+    for(let i = 0; i < str.length; i++) {
+        if (str.charAt(i) == ")") {
+            count += 1
+        }
+    }
+    for(let i = 0; i < str.length; i++) {
+        if (str.charAt(i) == "(") {
+            count2 += 1
+        }
+    }
+    if(count === count2) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(countString(sample4))
